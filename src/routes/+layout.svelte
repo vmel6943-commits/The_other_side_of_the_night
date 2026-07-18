a795da3848db009c2f1ef0a8df33f3cb5a0223f7
2388983944 <2388983944@qq.com>
2026-07-19 00:26:50 +0800

    Fix GitHub Pages font paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+	import { base } from "$app/paths";
 	import "$styles/app.css";
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="/assets/fonts/noto-sans-sc/wght.css" />
+	<link rel="stylesheet" href={`${base}/assets/fonts/noto-sans-sc/wght.css`} />
+	<link rel="stylesheet" href={`${base}/assets/fonts/site-fonts.css`} />
 </svelte:head>
 
 <slot />
